0a5d5e9421b2651b5fd700bdac32ccea0411e78b
Lubimov Igor <infunt@gmail.com>
2015-06-08 14:35:49 +0300

    upgrade to 0.0.6

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='checkio_executor_python',
-    version='0.0.5',
+    version='0.0.6',
     description='CheckiO module for execute python code',
     author='CheckiO',
     author_email='igor@checkio.org',
     url='https://github.com/CheckiO/checkio-executor-python',
-    download_url='https://github.com/CheckiO/checkio-executor-python/tarball/0.0.5',
+    download_url='https://github.com/CheckiO/checkio-executor-python/tarball/0.0.6',
     packages=find_packages(),
 )

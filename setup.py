40b9436d5ca79963c61ce6d4048b7be21bb75cf1
oduvan <a.lyabah@checkio.org>
2016-02-11 14:12:00 +0200

    new version

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='checkio_executor_python',
-    version='0.0.8',
+    version='0.0.9',
     description='CheckiO module for execute python code',
     author='CheckiO',
     author_email='igor@checkio.org',
     url='https://github.com/CheckiO/checkio-executor-python',
-    download_url='https://github.com/CheckiO/checkio-executor-python/tarball/0.0.8',
+    download_url='https://github.com/CheckiO/checkio-executor-python/tarball/0.0.9',
     packages=find_packages(),
 )
